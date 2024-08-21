8a75bbab2e396543836afb6182835eeac350f873
aliabbaschadhar <work.aliabbaschadhar@gmai.com>
2024-08-21 11:19:13 +0500

    Model Component

---
+++ src/components/Model.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Model() {
+	return (
+		<section className="common-padding">
+			<div className="screen-max-width">
+				<h1 id="heading" className="section-heading "></h1>
+			</div>
+		</section>
+	);
+}
+
+export default Model;
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Highlights from "./Highlights";
 import VideoCarousel from "./VideoCarousel";
+import Model from "./Model";
 
-export { Navbar, Hero, Highlights, VideoCarousel };
+export { Navbar, Hero, Highlights, VideoCarousel, Model };
