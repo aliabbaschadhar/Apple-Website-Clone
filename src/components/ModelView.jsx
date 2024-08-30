35fe2830c7f9746877569342192d5f842bf4d0b8
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-08-30 19:05:44 +0500

    start again

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,22 @@
+import { View } from "@react-three/drei";
+import React from "react";
+
+function ModelView({
+	index,
+	groupRef,
+	gsapType,
+	controlRef,
+	setRotationSize,
+	size,
+	item,
+}) {
+	return (
+		<View
+			index={index}
+			id={gsapType}
+			className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%]' : '' `}
+		></View>
+	);
+}
+
+export default ModelView;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Hero from "./Hero";
 import Highlights from "./Highlights";
 import VideoCarousel from "./VideoCarousel";
 import Model from "./Model";
+import ModelView from "./ModelView";
 
-export { Navbar, Hero, Highlights, VideoCarousel, Model };
+export { Navbar, Hero, Highlights, VideoCarousel, Model, ModelView };
