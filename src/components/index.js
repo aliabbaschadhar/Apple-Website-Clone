ef42db76dc49e798bc2c8d5d684308a7ccf9fe71
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-09-08 12:12:26 +0500

    add few components

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,18 @@ import Highlights from "./Highlights";
 import VideoCarousel from "./VideoCarousel";
 import Model from "./Model";
 import ModelView from "./ModelView";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import Loader from "./Loader";
 
-export { Navbar, Hero, Highlights, VideoCarousel, Model, ModelView };
+export {
+	Navbar,
+	Hero,
+	Highlights,
+	VideoCarousel,
+	Model,
+	ModelView,
+	Lights,
+	IPhone,
+	Loader,
+};
